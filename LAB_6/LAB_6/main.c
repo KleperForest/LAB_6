5a3c2ee008c1fbf32e9fdc1fbed468e6b3d99217
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-04-19 15:29:05 -0600

    d

--- LAB_6/LAB_6/main.c
+++ LAB_6/LAB_6/main.c
@@ -1,3 +1,13 @@
+//*********************************************************************
+// Universidad del Valle de Guatemala
+// IE2023: Programación de Microcontroladores
+// Autor: Alan Gomez
+// Proyecto: LAB_6.c
+// Descripción: Sexto Laboratorio de Programación de Microcontroladores con lenguaje C.
+// Hardware: ATmega328p
+// Created: 4/18/2024 12:27:37 AM
+//*********************************************************************
+
 #define F_CPU 16000000
 
 #include <avr/io.h>
