afa54e474d3aef8a5209d014e4e1e7e99f55153b
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-04-19 15:41:19 -0600

    fail

--- LAB_6/LAB_6/main.c
+++ LAB_6/LAB_6/main.c
@@ -15,7 +15,6 @@
 // Declaración de funciones
 void initUART9600(void);
 void setup(void);
-void decimalToBinary(uint8_t decimal, uint8_t binary[]);
 
 // Variable global para almacenar el último byte recibido
 volatile uint8_t bufferRX = 0;
@@ -34,16 +33,9 @@ int main(void)
 		// Si se recibe un byte
 		if (bufferRX != 0)
 		{
-			// Convertir el byte recibido a su representación binaria y encender LEDs
-			uint8_t binary[8] = {0};
-			decimalToBinary(bufferRX, binary);
-			for (int i = 0; i < 6; i++)
-			{
-				if (binary[i])
-				PORTB |= (1 << i); // Establecer el bit correspondiente en el puerto B
-				else
-				PORTB &= ~(1 << i); // Borrar el bit correspondiente en el puerto B
-			}
+			// Mostrar el carácter recibido en los LEDs
+			PORTB = bufferRX;
+			
 			// Limpiar el buffer después de procesar el byte recibido
 			bufferRX = 0;
 		}
@@ -54,8 +46,8 @@ int main(void)
 // Configuración de pines
 void setup(void)
 {
-	// Configurar pines PB0 a PB5 como salidas para LEDs
-	DDRB |= 0b00111111;
+	// Configurar puerto B como salida para LEDs
+	DDRB = 0xFF;
 }
 
 // Inicialización de UART a 9600 baudios
@@ -66,7 +58,7 @@ void initUART9600()
 	DDRD |= (1 << DDD1);   // TX como salida
 
 	// Habilitar recepción y transmisión
-	UCSR0B = (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0);
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 
 	// Configurar velocidad de baudios a 9600
 	UBRR0 = 103;
@@ -80,20 +72,4 @@ ISR(USART_RX_vect)
 {
 	// Almacenar el byte recibido en el buffer
 	bufferRX = UDR0;
-
-	// Enviar el byte de vuelta
-	while (!(UCSR0A & (1 << UDRE0)));
-	UDR0 = bufferRX;
-}
-
-// Función para convertir decimal a binario
-void decimalToBinary(uint8_t decimal, uint8_t binary[])
-{
-	int i = 0;
-	while (decimal > 0)
-	{
-		binary[i] = decimal % 2;
-		decimal = decimal / 2;
-		i++;
-	}
 }
